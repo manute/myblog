aac1347b6d06b38b3eab7d734fedf4c4a0f8c54d
Manuel Alonso <manuteali@gmail.com>
2012-02-04 20:00:08 +0100

    Add Rich Editor and change index with html_safe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order(:published_on)
 
     respond_to do |format|
       format.html # index.html.erb
